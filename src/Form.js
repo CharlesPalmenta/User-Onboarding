82bc65388a396acf1e63541cab5d71e7e17646de
Charles Palmenta <palmentac@gmail.com>
2020-06-09 22:06:41 -0400

    adding onChange and value to inputs

--- src/Form.js
+++ src/Form.js
@@ -10,11 +10,39 @@ export default function Form() {
        system: "",
        terms: true 
     });
+    const [buttonDisabled, setButtonDisable] = useState(true)
+    const formSchema = yup.object().shape({
+        name: yup.string().required("Name is a required field"),
+        email: yup.string().email("Must be a valid email address").required("Must include email"),
+        password: yup.string().min(6).required("Password must be at least 6 characters"),
+        system: yup.mixed().oneOf(["mac", "windows", "linux"]).defined(),
+        terms: yup.boolean().oneOf([true])
+    })
+
+    useEffect(() => {
+        console.log("checking for form validation")
+        formSchema.isValid(formState).then(valid => {
+            console.log("is form valid?", valid)
+            setButtonDisable(!valid)
+        })
+    }, [formState]);
+
+    const inputChange = e => {
+        e.persist();
+        console.log("changed", e.target.value);
+        console.log("name of input that fired event", e.target.name);
+        const newFormData = {
+            ...formState,
+            [e.target.name]:e.target.name === "terms" ? e.target.checked : e.target.value
+        }
+        setFormState(newFormData);
+    }
+
     return (
         <form>
             <label htmlFor="name">
                 Name
-                <input id="name" type="text" name="name" />
+                <input id="name" type="text" name="name" value={formState.name} onChange={inputChange} />
             </label>
             <label htmlFor="email">
                 Email
@@ -37,7 +65,7 @@ export default function Form() {
                 Terms and Conditions
                 <input id="terms" type="checkbox" name="terms" checked={true} />
             </label>
-            <button>Submit</button>
+            <button disabled={buttonDisabled}>Submit</button>
         </form>
     )
 }
